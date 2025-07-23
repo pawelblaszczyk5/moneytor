06d493d1c1162a72f456248c1f7e9abefec4db7b
Paweł Błaszczyk <internal@pawelblaszczyk.dev>
2025-07-23 18:53:45 +0200

    ci: remove commonality (#21)
    I want to get rid of commonality, it's unmaintained and doesn't provide
    so much value. Instead I will use boundaries from turbo + I want to
    leverage turbo generators

--- tooling/commonality/eslint.config.js
+++
@@ -1,7 +0,0 @@
-import core from "@moneytor/eslint-config/core";
-
-export default [
-	{ languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname } } },
-	...core,
-	{ files: ["src/*.ts"], rules: { "import-x/no-default-export": "off" } },
-];
