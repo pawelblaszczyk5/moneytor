8ea5a9a71ff4bf2991254b0dc44906373e914f6a
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-09-12 20:58:24 +0200

    feat: add assert package

---
+++ packages/assert/eslint.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "eslint/config";
+
+import core from "@moneytor/eslint-config/core";
+
+export default defineConfig(
+	{ languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname } } },
+	{ extends: [core], files: ["**/*.{ts,tsx,js,jsx}"] },
+);
