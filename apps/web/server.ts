83b24f8a0a807a1aa9a7dfa1d148c7405d201dce
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-10-07 22:21:55 +0200

    feat: use Hono instead of srvx

--- apps/web/server.ts
+++ apps/web/server.ts
@@ -1,8 +1,30 @@
-import { serve } from "srvx/node";
-import { serveStatic } from "srvx/static";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { Hono } from "hono";
+import { createMiddleware } from "hono/factory";
 
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports -- that one is fine
 import handler from "./dist/server/server.js";
 
-const server = serve({ fetch: handler.fetch, middleware: [serveStatic({ dir: "./dist/client" })], port: 3200 });
+const ONE_YEAR_IN_SECONDS = 60 * 60 * 24 * 365;
+const ONE_HOUR_IN_SECONDS = 60 * 60;
 
-await server.ready();
+const cache = ({ immutable, seconds }: { immutable: boolean; seconds: number }) =>
+	createMiddleware(async (ctx, next) => {
+		await next();
+
+		if (!ctx.res.ok || ctx.res.headers.has("cache-control")) {
+			return;
+		}
+
+		ctx.res.headers.set("cache-control", `public, max-age=${seconds.toString()}${immutable ? ", immutable" : ""}`);
+	});
+
+const app = new Hono();
+
+app.use("/assets/*", cache({ immutable: true, seconds: ONE_YEAR_IN_SECONDS }), serveStatic({ root: "./dist/client" }));
+app.use("*", cache({ immutable: false, seconds: ONE_HOUR_IN_SECONDS }), serveStatic({ root: "./dist/client" }));
+
+app.use("*", async (ctx) => handler.fetch(ctx.req.raw) as Promise<Response>);
+
+serve({ fetch: app.fetch, port: 3_200 });
